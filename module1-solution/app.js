7d6367d62bcdd1bea59ede638a7e852ccef2fb03
carlos eduardo gonçalves <kherokuone@rediffmail.com>
2016-09-18 02:38:56 -0300

    final

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,10 +1,22 @@
 (function () {
-  'use strict';
-  angular.module('app', []).controller('controller', function ($scope) {
-    $scope.showMessage = function () {
-      var tb = $scope.textbox;
+'use strict';
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.showMessage = function () {
+    var tb = $scope.textbox;
+    if (tb == "" || tb == null) {
+      $scope.message = "Please enter data first";
+    }else{
       var tbList = tb.split(",");
-      $scope.message = tbList.length;
-    };
-  });
+      if (tbList.length <= 3) {
+        $scope.message = "Enjoy!";
+      }else{
+        $scope.message = "Too much!";
+      }
+    }
+  };
+}
 })();
