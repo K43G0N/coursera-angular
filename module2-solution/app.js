2416f43ff4ef4655ba68420fd2e6245b37a12f79
carlos eduardo gonçalves <kherokuone@rediffmail.com>
2016-09-26 00:24:49 -0300

    first

---
+++ module2-solution/app.js
@@ -0,0 +1,66 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var toBuyList = this;
+
+  toBuyList.items = ShoppingListCheckOffService.getItemsToBuy();
+
+  toBuyList.buyItem = function (index) {
+    ShoppingListCheckOffService.addItemToBoughtItems(index);
+    ShoppingListCheckOffService.removeItemFromToBuyItems(index);
+    toBuyList.message = ShoppingListCheckOffService.getMessage(1);
+  };
+}
+
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+  boughtList.message = function () {
+    return ShoppingListCheckOffService.getMessage(2);
+  };
+}
+
+function ShoppingListCheckOffService() {
+   var service = this;
+   var maxItems = 5;
+   var itemsToBuy = [
+     { name: "aspirina", quantity: "10" },
+     { name: "cookies", quantity: "8" },
+     { name: "coca-cola", quantity: "5" },
+     { name: "pepto bismol", quantity: "2" },
+     { name: "chocolate", quantity: "9" }
+   ];
+   var boughtItems = [];
+
+   service.addItemToBoughtItems = function (index) {
+     var countBroughtItems = "";
+     var countItemToBuy = "";
+     if (boughtItems.indexOf(itemsToBuy[index]) == -1) {
+       boughtItems.push(itemsToBuy[index]);
+     }
+   };
+   service.removeItemFromToBuyItems = function (index) {
+     itemsToBuy.slice(0).splice(index,1);
+   };
+   service.getItemsToBuy = function () {
+     return itemsToBuy;
+   };
+   service.getBoughtItems = function () {
+     return boughtItems;
+   };
+   service.getMessage = function (index) {
+     if (index == 1)
+       return (service.getBoughtItems().length > maxItems-1)
+    if (index == 2)
+      return (service.getBoughtItems().length == 0);
+   };
+}
+})();
